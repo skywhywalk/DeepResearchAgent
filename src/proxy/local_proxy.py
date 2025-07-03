8a7545034fcfe6b6a1d561e84107aa173b4a23c9
DVampire <1271421361@qq.com>
2025-07-03 20:46:15 +0800

    fix audio bugs

--- src/proxy/local_proxy.py
+++ src/proxy/local_proxy.py
@@ -1,15 +1,28 @@
 import os
 import httpx
-import openai
 import contextlib
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
+PROXY_URL = os.getenv('LOCAL_PROXY_BASE', None)
+
 HTTP_CLIENT = httpx.Client()
 ASYNC_HTTP_CLIENT = httpx.AsyncClient()
 
+@contextlib.contextmanager
+def proxy_env(proxy_url: str = PROXY_URL):
+    os.environ["HTTP_PROXY"] = proxy_url
+    os.environ["HTTPS_PROXY"] = proxy_url
+    try:
+        yield
+    finally:
+        del os.environ["HTTP_PROXY"]
+        del os.environ["HTTPS_PROXY"]
+
 __all__ = [
+    "PROXY_URL",
     "HTTP_CLIENT",
     "ASYNC_HTTP_CLIENT",
+    "proxy_env",
 ]
